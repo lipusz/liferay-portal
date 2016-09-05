257aaa57fb33f9f1835c1c93f8add39a916201a7
antonio-ortega <60252917@liferay.com>
2016-09-05 13:02:04 +0200

    LPS-67650 SF

--- portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
@@ -70,6 +70,7 @@ import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.trash.TrashHandler;
 import com.liferay.portal.kernel.trash.TrashRenderer;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashUtil;
 import com.liferay.portal.kernel.util.ListUtil;
@@ -1672,13 +1673,13 @@ public abstract class BaseIndexer<T> implements Indexer<T> {
 			attributeName);
 
 		SearchEngine searchEngine = SearchEngineHelperUtil.getSearchEngine(
-				searchContext.getSearchEngineId());
-		
-		if (searchEngine.getVendor().equalsIgnoreCase("Solr") &&
-				fieldName.contains(StringPool.SPACE)) {
-			
+			searchContext.getSearchEngineId());
+
+		if (StringUtil.equalsIgnoreCase(searchEngine.getVendor(), "Solr") &&
+			fieldName.contains(StringPool.SPACE)) {
+
 			fieldName = StringUtil.replace(
-				fieldName, StringPool.SPACE,
+				fieldName, CharPool.SPACE,
 				StringPool.BACK_SLASH + StringPool.SPACE);
 		}
 
