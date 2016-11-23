49d9c0d951045b6340ea4150d412540e45a08d16
rimi saadou <rimi.saadou@liferay.com>
2016-11-23 09:49:06 +0100

    LPP-23227 [DXP-GROW] Setting virtual host for private page breaks site control panel links

--- portal-impl/src/com/liferay/portal/service/impl/LayoutSetLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/LayoutSetLocalServiceImpl.java
@@ -469,8 +469,11 @@ public class LayoutSetLocalServiceImpl extends LayoutSetLocalServiceBaseImpl {
 		}
 		else {
 			try {
-				virtualHostPersistence.removeByC_L(
-					layoutSet.getCompanyId(), layoutSet.getLayoutSetId());
+				virtualHostLocalService.updateVirtualHost(
+					layoutSet.getCompanyId(), layoutSet.getLayoutSetId(), null);
+				VirtualHost VirtualHostToRemove =
+					virtualHostPersistence.removeByC_L(
+						layoutSet.getCompanyId(), layoutSet.getLayoutSetId());
 			}
 			catch (NoSuchVirtualHostException nsvhe) {
 
