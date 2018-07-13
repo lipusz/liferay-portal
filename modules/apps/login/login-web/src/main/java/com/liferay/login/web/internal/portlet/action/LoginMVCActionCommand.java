06bb421cdc7f67454f41cb32f3c1475138a1592b
NemethNorbert <norbert.nemeth@liferay.com>
2018-07-13 11:54:21 +0200

    LPS-83318 check the original window state on the loginPortlet before Maximizing.

--- modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/LoginMVCActionCommand.java
+++ modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/action/LoginMVCActionCommand.java
@@ -297,7 +297,8 @@ public class LoginMVCActionCommand extends BaseMVCActionCommand {
 			SessionErrors.add(actionRequest, "login", login);
 		}
 
-		if (portletName.equals(LoginPortletKeys.LOGIN)) {
+		if (portletName.equals(LoginPortletKeys.LOGIN) &&
+				(actionRequest.getWindowState() != WindowState.NORMAL)) {
 			portletURL.setWindowState(WindowState.MAXIMIZED);
 		}
 		else {
