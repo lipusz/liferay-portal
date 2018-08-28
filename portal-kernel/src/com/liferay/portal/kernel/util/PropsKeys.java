cb3ec035798282fa29d687b5e11fc2cea48f0f22
Peter Petrekanics <peter.petrekanics@liferay.com>
2018-08-28 12:49:30 +0200

    "LPS-77366 Updated PropsKeys and PropsValues with the new permissions.check.roles.enabled property"

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1831,6 +1831,8 @@ public interface PropsKeys {
 
 	public static final String PERMISSIONS_CHECK_GUEST_ENABLED = "permissions.check.guest.enabled";
 
+	public static final String PERMISSIONS_CHECK_ROLES_ENABLED = "permissions.check.roles.enabled";
+
 	public static final String PERMISSIONS_CHECKER = "permissions.checker";
 
 	public static final String PERMISSIONS_CUSTOM_ATTRIBUTE_READ_CHECK_BY_DEFAULT = "permissions.custom.attribute.read.check.by.default";
