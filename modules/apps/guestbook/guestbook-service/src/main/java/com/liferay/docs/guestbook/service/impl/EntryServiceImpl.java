15bd8972e7bbb516305d8b1200c47b533545326d
Tibor Lipusz <tibor.lipusz@liferay.com>
2019-03-13 18:09:19 +0100

    LPS-7.2 Add 'search' method to EntryServiceImpl to play with Search APIs

--- modules/apps/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/EntryServiceImpl.java
+++ modules/apps/guestbook/guestbook-service/src/main/java/com/liferay/docs/guestbook/service/impl/EntryServiceImpl.java
@@ -14,7 +14,26 @@
 
 package com.liferay.docs.guestbook.service.impl;
 
+import com.liferay.docs.guestbook.model.Entry;
 import com.liferay.docs.guestbook.service.base.EntryServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.search.document.Document;
+import com.liferay.portal.search.legacy.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.query.BooleanQuery;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.searcher.SearchRequest;
+import com.liferay.portal.search.searcher.SearchRequestBuilder;
+import com.liferay.portal.search.searcher.SearchResponse;
+import com.liferay.portal.search.searcher.Searcher;
+import com.liferay.portal.spring.extender.service.ServiceReference;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * The implementation of the entry remote service.
@@ -36,4 +55,55 @@ public class EntryServiceImpl extends EntryServiceBaseImpl {
 	 *
 	 * Never reference this class directly. Always use {@link com.liferay.docs.guestbook.service.EntryServiceUtil} to access the entry remote service.
 	 */
+
+	public List<Entry> search(long companyId) throws PortalException {
+		BooleanQuery booleanQuery = _queries.booleanQuery();
+
+		booleanQuery.addMustQueryClauses(
+			_queries.term(Field.COMPANY_ID, companyId));
+
+		SearchRequestBuilder searchRequestBuilder =
+			_searchRequestBuilderFactory.getSearchRequestBuilder(
+				new SearchContext());
+
+		SearchRequest searchRequest = searchRequestBuilder.entryClassNames(
+			Entry.class.getName()
+		).modelIndexerClasses(
+			Entry.class
+		).query(
+			booleanQuery
+		).withSearchContext(
+			searchContext -> searchContext.setCompanyId(companyId)
+		).build();
+
+		SearchResponse searchResponse = _searcher.search(searchRequest);
+
+		return getEntries(searchResponse);
+	}
+
+	protected List<Entry> getEntries(SearchResponse searchResponse) {
+		Stream<Document> documentStream = searchResponse.getDocumentsStream();
+
+		return documentStream.map(
+			document -> document.getFieldValue(Field.ENTRY_CLASS_PK)
+		).map(
+			GetterUtil::getLong
+		).map(
+			entryLocalService::fetchEntry
+		).filter(
+			Objects::nonNull
+		).collect(
+			Collectors.toList()
+		);
+	}
+
+	@ServiceReference(type = Queries.class)
+	private Queries _queries;
+
+	@ServiceReference(type = Searcher.class)
+	private Searcher _searcher;
+
+	@ServiceReference(type = SearchRequestBuilderFactory.class)
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
 }
