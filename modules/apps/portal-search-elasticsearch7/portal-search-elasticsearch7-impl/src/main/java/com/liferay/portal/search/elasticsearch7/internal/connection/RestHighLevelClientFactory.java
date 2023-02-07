115eb790cc67b2dfe85d123942194c678dbc20c9
Tibor Lipusz <tibor.lipusz@liferay.com>
2023-02-07 17:51:02 +0100

    LPS-174975 portal-search-elasticsearch7-impl: Default to true in api compatibility TEMP
    https://issues.liferay.com/browse/LPS-174975
    
    Signed-off-by: Tibor Lipusz <tibor.lipusz@liferay.com>

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/connection/RestHighLevelClientFactory.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/connection/RestHighLevelClientFactory.java
@@ -78,7 +78,7 @@ public class RestHighLevelClientFactory {
 			() -> new RestHighLevelClientBuilder(
 				restClientBuilder.build()
 			).setApiCompatibilityMode(
-				_apiCompatibilityModeEnabled
+				true
 			).build(),
 			getClass());
 	}
